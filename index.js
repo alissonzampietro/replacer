e16ec9761179c5e8d687be617a480453ce0fb7a8
Alisson Zampietro <Alisson Zampietro>
2018-09-10 10:31:03 -0300

    fix: special characters

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const fs = require('fs')
 app.get('/', (req, res) => {
     var find = ["ã","à","á","ä","â","è","é","ë","ê","ì","í","ï","î","ò","ó","ö","ô","ù","ú","ü","û","ñ","ç"];
     var replace = ["a","a","a","a","a","e","e","e","e","i","i","i","i","o","o","o","o","u","u","u","u","n","c"];
-
+    let flag = false
     fs.readFile('./input/file.txt','utf8',(err,data) => {
+        if(flag == false) {
+            v1 = data;
+            flag = true;
+        }
         for (var i = 0; i < find.length; i++) {
             console.log(data.replace(new RegExp(find[i], 'gi'), replace[i].toUpperCase()))
-            v1 = data || v1
-            v1 = data.replace(new RegExp(find[i], 'gi'), replace[i].toUpperCase());
+            v1 = v1.replace(new RegExp(find[i], 'gi'), replace[i].toUpperCase());
         }
 
         fs.writeFile('./output/output.txt', v1,'utf8',(err) => {
